3ecb8b6ceef36adaf018d5120e3a44642eb34936
ScooterMcgavin21 <scott.skrobel@gmail.com>
2022-04-23 15:11:53 -0600

    webpack cleanup, set up node scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const plugins = [
+  new CleanWebpackPlugin(),
   new HtmlWebpackPlugin({
     template: './src/index.html'
   })
 ];
+
 let mode = 'development';
 if (process.env.NODE_ENV === 'production') {
   mode = 'production';
 }
+
 module.exports = {
   mode: mode,
   entry: './src/index.js',
@@ -28,6 +31,10 @@ module.exports = {
         }
       }
     ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
 
   plugins: plugins,
